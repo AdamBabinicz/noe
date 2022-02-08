d9300926c0da23a0231b9dbdd343ee93183329c6
babinicz <babinicz@onet.pl>
2022-02-08 18:51:42 +0100

    z

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Skills from "./components/Skills";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import { motion } from "framer-motion";
+import CookieConsent from "react-cookie-consent";
 
 function App() {
   return (
@@ -27,6 +28,33 @@ function App() {
       <Pricing />
       <Contact />
       <Footer />
+      <CookieConsent
+        debug={true}
+        location="bottom"
+        style={{
+          background: "#333",
+          textAlign: "left",
+          paddingBottom: "1rem",
+          fontSize: "16px",
+          fontFamily: "Gideon Roman",
+        }}
+        buttonStyle={{
+          color: "#333",
+          background: "#fff",
+          fontSize: "18px",
+          fontFamily: "Gideon Roman",
+          marginRight: "1rem",
+        }}
+        buttonText="OK, rozumiem"
+        expires={365}
+      >
+        "W ramach naszej witryny stosujemy pliki cookies w celu świadczenia
+        Państwu usług na najwyższym poziomie, w tym w sposób dostosowany do
+        indywidualnych potrzeb. Korzystanie z witryny bez zmiany ustawień
+        dotyczących cookies oznacza, że będą one zamieszczane w Państwa
+        urządzeniu końcowym. Możecie Państwo dokonać w każdym czasie zmiany
+        ustawień dotyczących cookies."
+      </CookieConsent>
     </motion.div>
   );
 }
