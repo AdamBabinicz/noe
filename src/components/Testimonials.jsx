d35d16c0bd277aea0c1594c2f827e73a9c6e176d
babinicz <babinicz@onet.pl>
2022-02-09 12:52:33 +0100

    gr

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -204,7 +204,7 @@ const Section = styled.section`
   }
 
   @media screen and (min-width: 280px) and (max-width: 1080px) {
-    background: #ccc;
+    background: grey;
     .background {
       display: none;
     }
